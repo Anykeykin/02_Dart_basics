c3b425ff851dbca56276495180d4a8d387632550
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2021-12-30 22:55:07 +0300

    Create StrMethodSet.dart

---
+++ methods/StrMethodSet.dart
@@ -0,0 +1,27 @@
+class StrMethodSet {
+  final numStr = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine'
+  };
+  final str = '';
+  Set<int> set = {};
+  numSearch(str) {
+    final strArr = str.split(', ');
+    for (int el = 0; el < strArr.length; el++) {
+      for (int elem = 0; elem < numStr.length; elem++) {
+        if (strArr[el] == numStr[elem]) {
+          set.add(numStr.keys.elementAt(elem));
+        }
+      }
+    }
+    return 'Строка с цифрами и словами: $strArr, резултат:$set';
+  }
+}
