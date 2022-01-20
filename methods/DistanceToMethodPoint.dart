57d64317de8e598e96b909c8a2d49a10b5186ce6
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2022-01-20 23:04:08 +0300

    Update DistanceToMethodPoint.dart

--- methods/DistanceToMethodPoint.dart
+++ methods/DistanceToMethodPoint.dart
@@ -1,5 +1,25 @@
+import 'dart:math';
+
 class Point {
-  distanceTo(str) {
-    return str;
+  late double ax;
+  double ay;
+  double az;
+  final bx = 5;
+  final by = 7;
+  final bz = 1;
+
+  Point.search(this.ax, this.ay, this.az) {
+    this.ax = ax;
+    this.ay = ay;
+    this.az = az;
+  }
+
+  factory Point.first(ax, ay, az) {
+    final str = 'hello';
+    return Point.search(ax, ay, az);
+  }
+  distanceTo() {
+    final sum = sqrt(pow(bx - ax, 2) + pow(by - ay, 2) + pow(bz - az, 2));
+    return sum;
   }
 }
