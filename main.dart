27a250856314713b3785c7ddf818a17179536611
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2022-01-20 23:03:50 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'methods/DelimetersCalculator.dart';
 import 'methods/StrMethodSet.dart';
+import 'methods/algSearchPow.dart';
 import 'methods/binToInt.dart';
 import 'methods/StrMethodNum.dart';
 import 'methods/StrMethodMap.dart';
@@ -10,20 +11,39 @@ void main() {
   print(DelimetersCalculator()
       .nodNok(DelimetersCalculator().a, DelimetersCalculator().b));
   print('');
+
   print('2 Задание');
   print(binIntHelper().intToBin(135));
   print(binIntHelper().binToInt(10000111));
   print('');
+
   print('3 Задание');
   print(strMethodNum().numSearch('hey 12 fre 4 for 65 yes 86 res'));
   print('');
+
   print('4 Задание');
   print(StrMethodMap().mapHelper(['Привет', 'Пока', 'Русский', 'Hello']));
   print('');
+
   print('5 Задание');
   print(StrMethodSet().numSearch(
       'one, two, cat, three, dog, four, three, big, seven, true, two'));
   print('');
+
+  print('#НЕДОДЕЛАНО');
   print('6 Задание');
-  print(Point().distanceTo('connection: true'));
+  print('Решение с именованным конструктором');
+  Point zero = Point.search(4, 3, 9);
+  print(zero.distanceTo());
+  print('Решение с factory конструктором');
+  // Point one = Point.first(4, 3, 9);
+  // print(one.distanceTo());
+  print('');
+
+  print('7 Задание');
+  print('Нахождение корня n степени');
+  print(20.searchNSqrt(6, 117649));
+  print('');
+
+  print('8 Задание');
 }
